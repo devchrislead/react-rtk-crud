d7678ab06bcbadb4dea30236e73c6115014fc093
devchrislead <christian.tipantuna.81@gmail.com>
2022-07-23 18:33:10 -0500

    first commit

---
+++ src/features/tasks/taskSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = [{
+    id: '1',
+    title: 'task1',
+    description: 'task1 description ',
+    completed: false,
+},
+{
+    id: '2',
+    title: 'task2',
+    description: 'task2 description ',
+    completed: true,
+}]
+export const taskslice = createSlice({
+    name: 'tasks',
+    initialState,
+    reducers: {
+        addTask: (state, action) => {
+            state.push(action.payload)
+
+        },
+        deleteTask: (state, action) => {
+            const taskFound = state.find(task => task.id === action.payload)
+            if (taskFound) {
+                state.splice(state.indexOf(taskFound), 1)
+            }
+
+        },
+        editTask: (state, action) => {
+            const { id, title, description } = action.payload
+            const foundTask = state.find(task => task.id === id)
+            if (foundTask) {
+                foundTask.title = title
+                foundTask.description = description
+            }
+
+        }
+
+    }
+})
+export const { addTask, deleteTask, editTask } = taskslice.actions
+export default taskslice.reducer
